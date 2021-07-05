5305345753ccb8f09eab4c41b75165b71fe70c3f
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2021-07-05 14:45:47 +0300

    Use CSVHelper to write CSV files, change CSV format

--- src/main/kotlin/fi/hsl/transitdata/eke_sink/FileNameFormatter.kt
+++ src/main/kotlin/fi/hsl/transitdata/eke_sink/FileNameFormatter.kt
@@ -3,10 +3,10 @@ package fi.hsl.transitdata.eke_sink
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-private const val FILE_NAME_PATTERN = "%s_%s_vehicle_%s.csv"
+private const val FILE_NAME_PATTERN = "%s_vehicle_%s"
 
 private val TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH")
 
-fun formatCsvFileName(messageType: String, time: LocalDateTime, unitNumber: String): String {
-    return FILE_NAME_PATTERN.format(time.format(TIME_FORMATTER), messageType, unitNumber)
+fun formatCsvFileName(time: LocalDateTime, unitNumber: String): String {
+    return FILE_NAME_PATTERN.format(time.format(TIME_FORMATTER), unitNumber)
 }
