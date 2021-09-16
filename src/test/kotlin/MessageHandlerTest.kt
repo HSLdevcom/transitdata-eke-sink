4c8befccf06cbac860ae407e63c87994ddfb16fa
Jaakko Malkki <Jaakko.Malkki@hsl.fi>
2021-09-16 12:37:59 +0300

    Fix message handler tests

--- src/test/kotlin/MessageHandlerTest.kt
+++ src/test/kotlin/MessageHandlerTest.kt
@@ -1,87 +1,83 @@
+import com.google.protobuf.ByteString
 import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doAnswer
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
+import com.typesafe.config.Config
+import fi.hsl.common.mqtt.proto.Mqtt
 import fi.hsl.common.pulsar.PulsarApplicationContext
 import fi.hsl.transitdata.eke_sink.MessageHandler
-import org.apache.pulsar.client.api.Consumer
-import org.apache.pulsar.client.api.MessageId
-import org.apache.pulsar.client.api.Producer
-import org.apache.pulsar.client.api.TypedMessageBuilder
+import org.apache.pulsar.client.api.*
+import org.bouncycastle.asn1.iana.IANAObjectIdentifiers.directory
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TemporaryFolder
 import org.mockito.Mockito
 import java.io.File
 import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.Paths
 import java.util.*
 import java.util.concurrent.CompletableFuture
+import kotlin.io.path.exists
+import kotlin.random.Random
+import kotlin.streams.toList
 
 class MessageHandlerTest {
+    @Rule
+    @JvmField val temp = TemporaryFolder()
 
     lateinit var mockContext: PulsarApplicationContext
     lateinit var mockConsumer: Consumer<ByteArray>
-    lateinit var mockProducer: Producer<ByteArray>
     lateinit var mockMessage : TypedMessageBuilder<ByteArray>
 
     @Before
-    fun before(){
+    fun before() {
         mockConsumer = mock<Consumer<ByteArray>>{
-            on{ acknowledgeAsync(any<MessageId>()) } doReturn (CompletableFuture<Void>())
+            on { acknowledgeAsync(any<MessageId>()) } doReturn (CompletableFuture<Void>())
             on { acknowledgeCumulativeAsync(any<MessageId>()) } doReturn(CompletableFuture())
         }
         mockMessage = mock<TypedMessageBuilder<ByteArray>>(stubOnly = true){
-            on{ sendAsync() } doReturn (CompletableFuture<MessageId>())
-            on{ property(any<String>(), any<String>()) } doReturn (it)
-            on{ value(any<ByteArray>()) } doReturn(it)
+            on { sendAsync() } doReturn (CompletableFuture<MessageId>())
+            on { property(any<String>(), any<String>()) } doReturn (it)
+            on { value(any<ByteArray>()) } doReturn(it)
         }
-        mockProducer = mock<Producer<ByteArray>>{
-            on{ newMessage() } doReturn(mockMessage)
+        val mockConfig = mock<Config> {
+            on { getBoolean(any()) } doReturn(false)
         }
         mockContext = mock<PulsarApplicationContext>{
-            on{ consumer } doReturn (mockConsumer)
-            on{ singleProducer } doReturn (mockProducer)
+            on { consumer } doReturn (mockConsumer)
+            on { config } doReturn(mockConfig)
         }
     }
 
     @Test
-    fun handleMessageTest(){
-        val directory = File("eke")
-        if(!directory.exists()) directory.mkdir()
-
-        val handler = MessageHandler(mockContext, directory.toPath())
-        handler.handleMessage(createFirstMqttMessage())
-        handler.ackMessages()
-        Mockito.verify(mockConsumer, Mockito.times(1)).acknowledgeCumulativeAsync(any<MessageId>())
-        val file = File(directory, "1970-01-01T00_stadlerUDP_vehicle_7777.csv")
-        assertTrue(file.exists())
-        File(directory, "1970-01-01T00_stadlerUDP_vehicle_7777.csv").inputStream().use {
-            inputStream ->
-            val string = Scanner(inputStream, StandardCharsets.UTF_8.name()).useDelimiter("\\A").next()
-            //Do some test
+    fun handleMessageTest() {
+        val directory = temp.newFolder("eke").toPath()
+        if (Files.notExists(directory)) {
+            Files.createDirectories(directory)
         }
-        file.delete()
-        directory.delete()
+
+        val handler = MessageHandler(mockContext, directory)
+        handler.handleMessage(mock<Message<Any>> {
+            on { data } doAnswer { Mqtt.RawMessage.newBuilder().setPayload(ByteString.copyFrom(getMessageContent())).setTopic("eke/v1/sm5/15/A/stadlerUDP").setSchemaVersion(1).build().toByteArray() }
+            on { properties } doReturn(Collections.singletonMap(fi.hsl.common.transitdata.TransitdataProperties.KEY_SOURCE_MESSAGE_TIMESTAMP_MS, java.time.Instant.now().toEpochMilli().toString()))
+            on { messageId } doReturn(mock<MessageId>())
+        })
+
+        Thread.sleep(1000)
+
+        val directoryContent = Files.list(directory).toList()
+        //There should be one file
+        assertEquals(1, directoryContent.size)
+
+        handler.ackMessages(Files.list(directory).findAny().get().toAbsolutePath())
+        Mockito.verify(mockConsumer, Mockito.times(1)).acknowledgeAsync(any<MessageId>())
     }
 
-    @Test
-    fun handleMessagesTest(){
-        val directory = File("eke")
-        if(!directory.exists()) directory.mkdir()
-
-        val handler = MessageHandler(mockContext, directory.toPath())
-        getInputStreamFromTestFile().use { inputStream ->
-            while(inputStream.available() > 0){
-                val message = createMqttMessage(inputStream, "eke/v1/sm5/7777/stadlerUDP")
-                handler.handleMessage(message)
-            }
-        }
-        handler.ackMessages()
-        Mockito.verify(mockConsumer, Mockito.times(1)).acknowledgeCumulativeAsync(any<MessageId>())
-
-        val file = File(directory, "1970-01-01T00_stadlerUDP_vehicle_7777.csv")
-        assertTrue(file.exists())
-        file.delete()
-        directory.delete()
-    }
+    private fun getMessageContent(): ByteArray = this::class.java.getResourceAsStream("stadlerUDP.dat").use { it.readBytes() }
 }
 
